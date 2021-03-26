c0a65f1538ba0ba798879172bf9d1f41c0b6426d
Abiodun <abiodundev@gmail.com>
2021-03-26 15:41:06 +0100

    chore: setting up the database connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { MONGO_URI } = process.env;
+
+mongoose
+  .connect(
+    'mongodb+srv://abiodundev10620:Abiodun96@cluster0.xbvl0.mongodb.net/grazacInvestmentDB?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Database connected successful'))
+  .catch(err => console.log(err));
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 2020;
+const port = process.env.PORT || 2020;
 
-app.listen(PORT, () => {
-  console.log(`The app is running on PORT ${PORT}`);
+app.listen(port, () => {
+  console.log(`The app is running on PORT ${port}`);
 });
